91ce89670ef1ddc4d270eb1a7b855ef818c5aa30
mahesanchis <mahesa@floridauniversitaria.es>
2021-01-14 10:53:39 +0100

    modifico gestiondatos.java

--- src/model/GestionDatos.java
+++ src/model/GestionDatos.java
@@ -1,4 +1,5 @@
 package model;
+// aÃ±ado cualquier cosa
 
 public class GestionDatos {
 
@@ -6,17 +7,17 @@ public class GestionDatos {
 
 	}
 
-	//TODO: Implementa una función para abrir ficheros
+	//TODO: Implementa una funciï¿½n para abrir ficheros
 	
-	//TODO: Implementa una función para cerrar ficheros
+	//TODO: Implementa una funciï¿½n para cerrar ficheros
 	
 	public boolean compararContenido (String fichero1, String fichero2){
-		//TODO: Implementa la función
+		//TODO: Implementa la funciï¿½n
 		return true;
 	}
 	
 	public int buscarPalabra (String fichero1, String palabra, boolean primera_aparicion){
-		//TODO: Implementa la función
+		//TODO: Implementa la funciï¿½n
 		return 1;
 	}	
 
